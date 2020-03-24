dea0d5a5e4c55eed0be736eaf4ca4a5feaecefc4
Sammy Guo <masoug@gmail.com>
2020-03-24 11:13:46 -0700

    * set up externals repos * refactoring setup code

---
+++ MappingParams.hpp
@@ -0,0 +1,24 @@
+#ifndef MappingParams_h
+#define MappingParams_h
+
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
+
+#include <pointmatcher/PointMatcher.h>
+
+using PM = PointMatcher<float>;
+using DP = PM::DataPoints;
+using TP = PM::TransformationParameters;
+
+
+struct MappingParameters {
+    std::vector<DP> input_datapoints;
+};
+
+
+boost::optional<MappingParameters> setup(const int argc, const char *argv[]);
+
+
+#endif /* MappingParams_h */
--- main.cpp
+++ main.cpp
@@ -2,74 +2,8 @@
 #include <map>
 #include <vector>
 
-#include <boost/program_options.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/optional.hpp>
+#include "MappingParams.hpp"
 
-#include <pointmatcher/PointMatcher.h>
-
-
-using PM = PointMatcher<float>;
-using DP = PM::DataPoints;
-using TP = PM::TransformationParameters;
-
-
-struct MappingParameters {
-    std::vector<DP> input_datapoints;
-};
-
-boost::optional<MappingParameters> setup(const int argc, const char *argv[]) {
-    using namespace boost::program_options;
-    using namespace boost::filesystem;
-
-    options_description desc("Allowed Options");
-    desc.add_options()
-            ("help", "Show help message")
-            ("input_dir",
-             value<std::string>()->required(),
-             "Input directory listing of pointcloud ply files.");
-
-    variables_map vm;
-    store(parse_command_line(argc, argv, desc), vm);
-
-    if (vm.count("help")) {
-        std::cout << "Point cloud reconstruction from multiple RGB-D camera frames." << std::endl;
-        std::cout << desc << std::endl;
-        return {};
-    }
-
-    notify(vm);
-
-    const auto input_dir = path(vm["input_dir"].as<std::string>());
-    std::cout << "Loading input point clouds from " << input_dir << std::endl;
-    if (not is_directory(input_dir)) {
-        std::cerr << "Input directory argument " << input_dir << " is not a directory" << std::endl;
-        return {};
-    }
-
-    // parse list input ply files, then sort by timestamp
-    std::map<size_t, std::string> input_pointcloud_filenames;
-    for (directory_iterator itr(input_dir); itr != directory_iterator{}; itr++) {
-        const auto& p = itr->path();
-
-        if (not is_regular_file(p))
-            continue;
-
-        if (p.extension() != ".ply")
-            continue;
-
-        input_pointcloud_filenames[std::stol(p.stem().string())] = p.string();
-    }
-
-    MappingParameters result;
-    for (const auto& fname_pair : input_pointcloud_filenames) {
-        std::cout << "  Loading (ts " << fname_pair.first << ") " << fname_pair.second << std::endl;
-        result.input_datapoints.emplace_back(DP::load(fname_pair.second));
-    }
-    std::cout << "Loading done." << std::endl;
-
-    return result;
-}
 
 int main(const int argc, const char *argv[]) {
 
